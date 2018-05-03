47fec75e8ce50907f35a8f25d074c67a9a35600f
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-05-03 09:22:39 -0700

    sgfd

--- src/pages/elements/Foot.vue
+++ src/pages/elements/Foot.vue
@@ -17,7 +17,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .Fmain {
   height: 100px;
   width: 100%;
@@ -32,10 +32,7 @@ export default {
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
 }
 .fb {
-  background-image: url('../assets/face.svg');
-  background-repeat: no-repeat;
-  background-size: contain;
-  width: 100%;
+
 }
 h3 {
   color: #999;
